0e84b17bfc2cc8e3e46fb8cbfb5ce2c9b5d230c2
franklinz622 <franklinz622@gmail.com>
2018-10-02 10:00:06 -0400

    10/02

--- src/js/app.js
+++ src/js/app.js
@@ -5,37 +5,37 @@ import Projects from './handlers/projectHandler';
 
 //preload all thumbnails
 var assets = new Assets(
-	'futureofmusic',
-	'gravity',
-	'helloplaynye',
-	'jane',
-	'myth',
-	'offf',
-	'rescue',
-	'rone',
-	'magentaRiddim',
-	'thewalk'
+  'futureofmusic',
+  'gravity',
+  'helloplaynye',
+  'jane',
+  'myth',
+  'offf',
+  'rescue',
+  'rone',
+  'magentaRiddim',
+  'thewalk'
 );
 var projects;
 
 function init() {
-	var projectNum = 10; //
-	projects = new Projects(
-		projectNum, //number of projects
-		assets,
-		488, //project block width
-		244, //project block height
-		8, //gap width between projects blocks
-		2 //how many projects in a row
-	);
+  var projectNum = 10; //
+  projects = new Projects(
+    projectNum, //number of projects
+    assets,
+    488, //project block width
+    244, //project block height
+    8, //gap width between projects blocks
+    2 //how many projects in a row
+  );
 }
 
 function animate() {
-	requestAnimationFrame(animate);
-	projects.render();
+  requestAnimationFrame(animate);
+  projects.render();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	init();
-	animate();
+  init();
+  animate();
 });
